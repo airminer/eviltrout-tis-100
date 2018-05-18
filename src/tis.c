a224df29d501f2d9dba27616df2c7374ba745f9f
airminer <szepkuti.bence@gmail.com>
2018-05-18 02:14:12 +0100

    Add performance metrics

--- src/tis.c
+++ src/tis.c
@@ -6,9 +6,13 @@
 #include "util.h"
 #include "program.h"
 #include "output.h"
+#include <sys/time.h>
 
 
 int main(int argc, char **argv) {
+  struct timeval  tv1, tv2;
+  gettimeofday(&tv1, NULL);
+
   Program program;
   Program *p = &program;
 
@@ -20,14 +24,17 @@ int main(int argc, char **argv) {
 #endif
 
   program_init(p);
-  program_load_system(p, "./programs/divide.sys");
-  program_load_code(p, "./programs/divide.tis");
+  program_load_system(p, "refimpl.sys");
+  program_load_code(p, "refimpl.tis");
+
+  int tickcount = 0;
 
 #ifdef RICH_OUTPUT
   output_program(p);
   int code = getch();
   while (code != 'q') {
     program_tick(p);
+    tickcount++;
     clear();
     output_program(p);
     refresh();
@@ -37,6 +44,7 @@ int main(int argc, char **argv) {
   int all_blocked_count = 0;
   while (all_blocked_count < 5) {
     int all_blocked = program_tick(p);
+    tickcount++;
     if (all_blocked) {
       all_blocked_count++;
     } else {
@@ -49,4 +57,10 @@ int main(int argc, char **argv) {
   endwin();
 #endif
   program_clean(p);
+
+
+  gettimeofday(&tv2, NULL);
+  printf ("Clock cycles computed: %d\nElapsed milliseconds: %f\n", tickcount,
+    (double) (tv2.tv_usec - tv1.tv_usec) / 1000 +
+    (double) (tv2.tv_sec - tv1.tv_sec) * 1000);
 }
